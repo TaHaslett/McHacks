50ca148e004dac326ac713d40dd51d50409fbb92
tahaslett <tahaslett@videotron.ca>
2023-01-28 16:49:00 -0500

    modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,10 @@
 import json
 
+import requests
+
 def create_restaurant_dict(filename: str):
     fobj = open(filename, "r")
     data = json.load(fobj)
-    
-    for i in data:
-        print(i)
        
     return data
 
@@ -13,15 +12,22 @@ def cheapest_list(data_set, search_item, length):
     
     top_list = []
     while len(top_list) <= length:
-        for restaurant, menu in enumerate(data_set):
+        for restaurant, menu in data_set.items():
             if restaurant in top_list:
                 continue
             
-            for item, price in enumerate(menu):
+            for item, price in menu.items():
                 if len(top_list) == 0 or search_item in item and price <= cheapest_price:
                     cheapest_price = price
                     top_list.append((restaurant, item, price))
 
             break
+    
+    return top_list
+
 if __name__ == "__main__":
-    create_restaurant_dict("restaurants.json")
+    data = create_restaurant_dict("restaurants.json")
+
+    print(cheapest_list(data, "cheese", 3))
+    
+    pass
