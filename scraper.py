72e5b7107cfb57ddc6b24647b2bceff4d90a10af
tahaslett <tahaslett@videotron.ca>
2023-01-28 17:39:30 -0500

    modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -8,6 +8,29 @@ def create_restaurant_dict(filename: str):
        
     return data
 
+def find_restaurants(data_set, search_item):
+    restaurants = []
+    
+    for restaurant, menu in data_set.items():
+        
+        for item, price in menu.values():
+            if search_item in item:
+                restaurants.append({restaurant: (item, price)})
+                
+    return restaurants
+            
+def find_cheapest(data_set, search_item):
+    restaurant_list = find_restaurants(data_set, search_item)
+    
+    for restaurant in restaurant_list:
+        
+        for item, price in data_set[restaurant].items():
+            if search_item in item and price <= cheapest_price:
+                cheapest_price = price
+                cheapest_item = item
+            
+        return restaurant, cheapest_item, cheapest_price
+
 def cheapest_list(data_set, search_item, length):
     
     top_list = []
@@ -20,6 +43,7 @@ def cheapest_list(data_set, search_item, length):
                 if len(top_list) == 0 or search_item in item and price <= cheapest_price:
                     cheapest_price = price
                     top_list.append((restaurant, item, price))
+                    
 
             break
     
@@ -28,6 +52,7 @@ def cheapest_list(data_set, search_item, length):
 if __name__ == "__main__":
     data = create_restaurant_dict("restaurants.json")
 
-    print(cheapest_list(data, "cheese", 3))
+    print(find_cheapest(data, "cheese"))
+    # print(cheapest_list(data, "cheese", 3))
     
     pass
