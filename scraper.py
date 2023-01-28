de4e9ab4b6058ca820c4ce089e28f3c359168406
tahaslett <tahaslett@videotron.ca>
2023-01-28 15:29:26 -0500

    modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -2,11 +2,12 @@ import json
 import requests
 
 def create_restaurant_dict(filename: str):
-    restaurant_dict = {}
-    
     fobj = open(filename, "r")
+    data = fobj.load(fobj)
     
-    contents = fobj.read().split("\n").split(",")
+    for i in data["restaurants"]:
+        print(i)
+        
+if __name__ == "__main__":
+    create_restaurant_dict("restaurants.json")
     
-    for line in contents:
-        restaurant_name = line[0]
