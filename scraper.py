76abf35849a7888caa844c673b424673ba13395c
tahaslett <tahaslett@videotron.ca>
2023-01-28 15:45:18 -0500

    modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -8,6 +8,18 @@ def create_restaurant_dict(filename: str):
     for i in data["restaurants"]:
         print(i)
         
+    return data
+
+def cheapest_list(data_set, food_item, length):
+    
+    top = []
+    while length(top) <= length:
+        for restaurant, menu in enumerate(data_set):
+            if restaurant in top:
+                continue
+            
+            for item in restaurant:
+                pass
+        
 if __name__ == "__main__":
     create_restaurant_dict("restaurants.json")
-    
