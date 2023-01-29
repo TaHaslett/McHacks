da296470ed3ab04d2a74f606d65aa1a352946305
tahaslett <tahaslett@videotron.ca>
2023-01-28 20:08:57 -0500

    modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -14,11 +14,12 @@ def find_restaurants(data_set, search_item):
     for restaurant, menu in data_set.items():
         
         items = {}
-        for item, price in menu.values():
-            if search_item in item:
+        for item, price in menu.items():
+            if search_item in item.lower():
                 items[item] = price
-                
-        restaurants[restaurant] = items
+        
+        if len(items) != 0:
+            restaurants[restaurant] = items
                 
     return restaurants
             
@@ -26,24 +27,26 @@ def find_cheapest(data_set, search_item):
     
     restaurants = find_restaurants(data_set, search_item)
     
-    overall_cheapest_restaurant = restaurants.keys()[0]
-    overall_cheapest_price = restaurants[0][0]
-    overall_cheapest_item = restaurants.keys()[0][0]
+    overall_cheapest_restaurant = list(restaurants.keys())[0]
+    overall_cheapest_item = list(list(restaurants.values())[0].keys())[0]
+    overall_cheapest_price = list(list(restaurants.values())[0].values())[0]
     
-    for restaurant in restaurants:
+    for menu in restaurants.values():
         
-        cheapest_price = restaurant[0]
-        cheapest_item = restaurant.keys()[0]
+        cheapest_item = list(menu.keys())[0]
+        cheapest_price = list(menu.values())[0]
         
-        for item, price in restaurant.values():
+        for item, price in menu.items():
             if price <= cheapest_price:
                 cheapest_price = price
-                cheapest_item = item
+                cheapest_item = item 
         
-        if cheapest_price <= overall_cheapest_item:
-            overall_cheapest_restaurant = restaurant
+        if cheapest_price <= overall_cheapest_price:
+            overall_cheapest_restaurant = menu
+            overall_cheapest_item = cheapest_item
+            overall_cheapest_price = cheapest_price
         
-    return overall_cheapest_restaurant, cheapest_item, cheapest_price
+    return overall_cheapest_restaurant, overall_cheapest_item, overall_cheapest_price
 
 def cheapest_list(data_set, search_item, length):
     cheapest_list = []
@@ -60,7 +63,10 @@ def cheapest_list(data_set, search_item, length):
 if __name__ == "__main__":
     data = create_restaurant_dict("restaurants.json")
 
-    print(cheapest_list(data, "cheese", 3))
+    # print(find_restaurants(data, "burger"))
+
+    print(find_cheapest(data, "cheese"))
+    
     # print(cheapest_list(data, "cheese", 3))
     
     pass
